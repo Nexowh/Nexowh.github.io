27e9a15bc22126fbb08763a022dacc3d1159d1cc
Nexowh <1207312421@qq.com>
2022-04-09 15:21:12 +0800

    Site updated: 2022-04-09 15:21:10

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+
+"CONFIG": {
+
+"modules": "01234",
+"background": "5",
+"tmpColor": "4A86E8",
+"tmpSize": "16",
+"cityColor": "FF9900",
+"citySize": "16",
+"aqiColor": "4A86E8",
+"aqiSize": "16",
+"weatherIconSize": "24",
+"alertIconSize": "18",
+"padding": "0px 0px 0px 0px",
+"shadow": "0",
+"language": "auto",
+"fixed": "true",
+"vertical": "center",
+"horizontal": "center",
+"left": "175",
+"top": "19",
+"key": "e6c48e7753604cc88923f6f5c7c3d526"
+}
+}
